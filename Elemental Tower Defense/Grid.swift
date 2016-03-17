3d7613c384a2b8245d89b174a81f76cec209df02
Andrew Turley <aturley123@gmail.com>
2016-03-17 12:18:21 -0400

    Grid class

---
+++ Elemental Tower Defense/Grid.swift
@@ -0,0 +1,17 @@
+//
+//  Grid.swift
+//  Elemental Tower Defense
+//
+//  Created by Andrew Turley on 3/17/16.
+//  Copyright © 2016 Polturonduken. All rights reserved.
+//
+
+import Foundation
+
+class Grid {
+    var towerList : [[Bool]] = [[]]
+    
+    init() {
+        
+    }
+}
